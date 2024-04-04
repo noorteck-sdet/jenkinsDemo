85c1f40c56615a3dea5ae376f579aa51114ea70f
SolianaYemane <155404423+SolianaYemane@users.noreply.github.com>
2024-04-04 14:54:04 -0700

    Update LoginStep.java

--- src/test/java/com/steps/LoginStep.java
+++ src/test/java/com/steps/LoginStep.java
@@ -14,7 +14,7 @@ public class LoginStep {
 	
 	WebDriver driver;
 	
-	//Solia
+	//Solia 2
 	@Given("User navigates to login page {string}")
 	public void user_navigates_to_login_page(String url) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
